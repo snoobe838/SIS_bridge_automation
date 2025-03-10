eecf0d7ab922525fe8c3639a6c48968860a2d084
snoobe838 <shubhampareek838@gmail.com>
2025-03-10 16:31:15 +0530

    addtion1

--- SIS_school config/pages/Add_student_page.py
+++ SIS_school config/pages/Add_student_page.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 import time
-import logging
 from pages.base_page import BasePage
 
 
@@ -11,9 +10,18 @@ class Addstudent(BasePage):
     
     SelectDist = (By.XPATH, "//input[@id='sch_dst_id']")
     SelectSchool = (By.XPATH, "//input[@id='school_realm_name']")
+    EnterEmail = (By.ID, "email")
+    EnterPass = (By.ID, "password")
+    EnterConfPass = (By.ID, "confirmPassword")
+    EnterFname = (By.ID, "first_name")
+    EnterLname = (By.ID, "last_name")
+    EnterDob = (By.ID, "dob")
+    SelectGender = (By.ID, "gender")
+    SelectEthnicity = (By.ID, "ethnicity")
+
     EmailInput = ()
 
-    def addstudent(self, dist_name, school_name, email):
+    def addstudent(self, dist_name, school_name, email, password, confirmpassword, firstname, lastname, dob, gender, ethnicity):
         self.logger.info("Navigating to SIS.")
         self.click(*self.Sis)
         self.logger.info("Navigated to SIS")
@@ -22,5 +30,13 @@ class Addstudent(BasePage):
         self.click(*self.SelectDist)
         self.send_keys_in_dropdown(*self.SelectDist, dist_name)
         self.send_keys_in_dropdown(*self.SelectSchool, school_name)
+        self.send_keys(*self.EnterEmail, email)
+        self.send_keys(*self.EnterPass, password)
+        self.send_keys(*self.EnterConfPass, confirmpassword)
+        self.send_keys(*self.EnterFname, firstname)
+        self.send_keys(*self.EnterLname, lastname)
+        self.send_keys(*self.EnterDob, dob)
+        self.select_dropdown_by_visible_text(*self.SelectGender, gender)
+        self.select_dropdown_by_visible_text(*self.SelectEthnicity, ethnicity)
         time.sleep(5)
 
--- SIS_school config/tests/test_add_student.py
+++ SIS_school config/tests/test_add_student.py
@@ -8,5 +8,5 @@ from pages.Add_student_page import Addstudent
 
 def test_add_student(driver):
     Add_student_page = Addstudent(driver)
-    Add_student_page.addstudent("_One_8", "A Godlike School", "")
+    Add_student_page.addstudent("_One_8", "A Godlike School", "snoobe+1@yopmail.com", "User@123", "User@123", "01/01/2000", "Male", "American Indian")
         
